2ad7b607d35747a819accf0f869d48eb71b3e2e1
nghiachaunamdinh <nghiachaunghiahung@gmail.com>
2026-08-22 14:34:05 +0700

    chore(frontend): add prettier

--- backend/src/modules/hop-dong/hop-dong.module.ts
+++ backend/src/modules/hop-dong/hop-dong.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { HopDong } from '../../entities/hop-dong.entity';
 import { HopDongController } from './hop-dong.controller';
 import { HopDongService } from './hop-dong.service';
+import { Giuong } from '../../entities/giuong.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([HopDong])],
+  imports: [TypeOrmModule.forFeature([HopDong,Giuong])],
   controllers: [HopDongController],
   providers: [HopDongService],
   exports: [HopDongService],
