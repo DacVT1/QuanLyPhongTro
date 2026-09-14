edcdc8b6fa82a61a60642853e334a1ad7110be3e
nghiachaunamdinh <nghiachaunghiahung@gmail.com>
2026-09-14 08:44:09 +0700

    postgress

--- frontend/src/components/hop-dong/HopDongPublic.vue
+++ frontend/src/components/hop-dong/HopDongPublic.vue
@@ -547,14 +547,6 @@ async function submitContract() {
 
     return;
   }
-  const response = await api.post("/public/hop-dong/submit", {
-    ...form.value,
-  });
-  successMessage.value =
-    response.data?.message ??
-    "Hợp đồng đã được tạo và gửi qua email thành công.";
-  submitting.value = true;
-  errorMessage.value = "";
 
   try {
     const token = new URLSearchParams(window.location.search).get("token");
